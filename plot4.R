8e866379433b5699a9a4bb3c579585f0831bf5d9
patmau <patmau@gmx.net>
2020-05-07 18:05:08 +0200

    plot 4

---
+++ plot4.R
@@ -0,0 +1,44 @@
+source("read_feb_data.R")
+
+fd <- read_feb_data()
+fd$DateTime <- strptime(paste(fd$Date, fd$Time), format = "%Y-%m-%d %H:%M:%S")
+
+png(filename = "plot4.png", width = 480, height = 480)
+
+par("mfrow" = c(2, 2))
+
+hist(fd$Global_active_power,
+     main = "",
+     col = "red",
+     xlab = "",
+     ylim = c(0, 1200)
+)
+
+plot(fd$DateTime, fd$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage"
+)
+
+plot(fd$DateTime, fd$Sub_metering_1,
+     type = "l",
+     xlab = "",
+     ylab = "Energy sub metering"
+)
+lines(fd$DateTime, fd$Sub_metering_2, col = "red")
+lines(fd$DateTime, fd$Sub_metering_3, col = "blue")
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"),
+       lty = c(rep("solid", 3)),
+       bty = "n"
+)
+
+plot(fd$DateTime, fd$Global_reactive_power,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global_reactive_power"
+)
+
+dev.off()
+
